a7cad8844dc3acb1ba8280842d74f55a9cf583a2
hamkke <walwalzz@naver.com>
2024-07-29 14:19:09 +0900

    tailwind 시작

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
 export default function Home() {
-  return <main>HELLO</main>;
+  return (
+    <main className='bg-blue-300 h-screen w-screen flex items-center justify-center p-5'>
+      <div className='bg-white p-5 shadow-2xl w-full rounded-2xl'>
+        <div>
+          <div>
+            <span>In transit</span>
+            <span>Coolblue</span>
+          </div>
+          <div />
+        </div>
+        <div>
+          <span>Today</span>
+          <span>9:30-10:30</span>
+        </div>
+        <div>
+          <div />
+          <div />
+        </div>
+        <div>
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span>Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
