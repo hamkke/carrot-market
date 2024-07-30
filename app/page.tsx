96925cdfc24db1c6b966a9eb529877acbed940f9
hamkke <walwalzz@naver.com>
2024-07-30 17:29:32 +0900

    #3.6 Form Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ export default function Home() {
       <div className='flex flex-col w-full max-w-screen-sm p-5 bg-white shadow-2xl md:flex-row rounded-2xl dark:bg-slate-500'>
         <input
           type='text'
+          required
           placeholder='keyword'
-          className='w-full h-10 px-4 mb-4 transition-shadow rounded-full outline-none md:m-0 bg-slate-100 ring ring-transparent focus:ring-rose-500 focus:ring-1 placeholder:text-rose-400'
+          className='w-full h-10 px-4 mb-4 transition-shadow rounded-full outline-none md:m-0 bg-slate-100 ring ring-transparent focus:ring-rose-500 focus:ring-2 placeholder:text-rose-600 invalid:bg-blue-400 invalid:focus:ring-black peer'
         />
+        <span className='hidden text-red-500 peer-invalid:block'>
+          빈칸 ㄴㄴ
+        </span>
         <button
           type='button'
-          className='py-2 text-white transition-colors rounded-full outline-none bg-gradient-to-br from-sky-400 via-rose-50 to-green-400 active:text-rose-500 focus:text-rose-500 md:ml-2 md:px-4'
+          className='py-2 text-white transition-colors bg-black rounded-full outline-none active:text-rose-500 focus:text-rose-500 md:ml-2 md:px-4 peer-invalid:bg-red-500 peer-required:bg-green-500'
         >
           SEARCH
         </button>
