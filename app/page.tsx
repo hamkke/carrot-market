0e751de011278ef495e56b3ae4c2abc6218b7834
hamkke <walwalzz@naver.com>
2024-07-30 14:42:02 +0900

    #3.5 Responsive Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <main className='flex items-center justify-center w-screen h-screen p-5 bg-blue-300 dark:bg-slate-700'>
-      <div className='flex flex-col w-full max-w-screen-sm p-5 bg-white shadow-2xl rounded-2xl dark:bg-slate-500'>
+    <main className='flex items-center justify-center w-screen h-screen p-5 bg-blue-900 dark:bg-slate-700 sm:bg-blue-700 md:bg-blue-500 lg:bg-blue-100 '>
+      <div className='flex flex-col w-full max-w-screen-sm p-5 bg-white shadow-2xl md:flex-row rounded-2xl dark:bg-slate-500'>
         <input
           type='text'
           placeholder='keyword'
-          className='w-full h-10 px-4 mb-4 transition-shadow rounded-full outline-none bg-slate-100 ring ring-transparent focus:ring-rose-500 focus:ring-1 placeholder:text-rose-400'
+          className='w-full h-10 px-4 mb-4 transition-shadow rounded-full outline-none md:m-0 bg-slate-100 ring ring-transparent focus:ring-rose-500 focus:ring-1 placeholder:text-rose-400'
         />
         <button
           type='button'
-          className='py-2 text-white transition-colors bg-black rounded-full outline-none active:text-rose-500 focus:text-rose-500'
+          className='py-2 text-white transition-colors bg-black rounded-full outline-none active:text-rose-500 focus:text-rose-500 md:ml-2 md:px-4'
         >
           SEARCH
         </button>
