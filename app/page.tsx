43880b5a425afbdb9ce5709d17a1b74972324795
hamkke <walwalzz@naver.com>
2024-08-05 14:12:41 +0900

    #3.11 Directives
    아직까지는
    .testApply { @apply }
    이거랑
    @layer components { .animatePing { @apply } }
    이것의 차이점을 모르겄다

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ export default function Home() {
       <div className='bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4'>
         {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
           <div key={index} className='flex items-center gap-5 group'>
-            <div className='size-[333px] bg-[#69886a] rounded-full group-hover:bg-lime-100 transition-all' />
+            <div className='size-[33px] bg-[#69886a] rounded-full group-hover:bg-lime-100 transition-all' />
             <span className='text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-blue-300'>
               {person}
             </span>
-            <div className='size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative'>
+            <div className='animatePing size6'>
               <span className='z-10'>{index}</span>
-              <div className='size-6 bg-violet-300 rounded-full absolute animate-ping ' />
+              <div className='testApply testApplyPlus' />
+            </div>
+            <div className='animatePing size10'>
+              <span className='z-10'>{index}</span>
+              <div className='testApply' />
             </div>
           </div>
         ))}
