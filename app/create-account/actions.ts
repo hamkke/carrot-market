70ea65d3f78827025893ad23496b4023e95039fa
hamkke <walwalzz@naver.com>
2024-08-08 10:56:00 +0900

    #6.2 Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,14 +1,36 @@
 'use server';
 
 import { z } from 'zod';
+const checkEmail = (email: string) => email.includes('@zod.com');
+const checkPassword = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
 
 // const usernameSchema = z.string().min(5).max(10);
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(3),
-  confirm_password: z.string().min(3),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: '온리 문자만 가능',
+        required_error: '이름을 알려주세요',
+      })
+      .min(3, '너무 짧아요')
+      .max(10, '너무 길어요'),
+    email: z
+      .string()
+      .email()
+      .refine(checkEmail, '@zod.com가 포함되어야 합니다'),
+    password: z.string().min(3),
+    confirm_password: z.string().min(3),
+  })
+  .refine(checkPassword, {
+    message: '둘이 달라요',
+    path: ['confirm_password'],
+  });
 
 export const createAccountAction = async (
   prevState: any,
