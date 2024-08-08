91e5ec2b20620ba9cc12b12635c75b7b2c1f83fa
hamkke <walwalzz@naver.com>
2024-08-08 15:22:55 +0900

    #6.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,22 +1,47 @@
 'use server';
 import { z } from 'zod';
 import validator from 'validator';
+import { redirect } from 'next/navigation';
 
-// import {
-//   MIN_LENGTH,
-//   PASSWORD_REGEX,
-//   PASSWORD_REGEX_ERROR,
-// } from '@/lib/constants';
+interface ActionState {
+  token: boolean;
+}
 
-// const formSchema = z.object({
-//   email: z.string().email(),
-//   password: z
-//     .string()
-//     .min(MIN_LENGTH, '너무 짧아요')
-//     .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-// });
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone_number) => validator.isMobilePhone(phone_number, 'ko-KR'),
+    'Wrong phone format'
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+export async function smsVerification(
+  prevState: ActionState,
+  formData: FormData
+) {
+  const phone_number = formData.get('phone_number');
+  const token = formData.get('token');
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone_number);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect('/');
+    }
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -13,8 +13,12 @@ import { smsVerification } from './actions';
 3. 사용자가 인증번호 작성
 4. 로그인
  */
+const initialState = {
+  token: false,
+  error: undefined,
+};
 export default function SMSLogin() {
-  const [state, action] = useFormState(smsVerification, null);
+  const [state, action] = useFormState(smsVerification, initialState);
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
@@ -22,21 +26,28 @@ export default function SMSLogin() {
         <h2 className='text-xl'>Verify your phone number.</h2>
       </div>
       <form className='flex flex-col gap-4' action={action}>
-        <Input
-          name='phone_number'
-          type='text'
-          placeholder='Phone number'
-          required
-        />
-        <Input
-          name='token'
-          type='number'
-          placeholder='Verification code'
-          required
-          min={100000}
-          max={999999}
-        />
-        <Btn text='Verify' />
+        {state.token ? (
+          <Input
+            name='token'
+            type='number'
+            placeholder='Verification code'
+            required
+            min={100000}
+            max={999999}
+            // 왜 fieldErrors가 아니지?
+            // smsVerification에서는 하나씩 검사했기 때문에 fromErrors
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            name='phone_number'
+            type='text'
+            placeholder='Phone number'
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Btn text={state.token ? '인증번호 확인' : '인증번호 요청하기'} />
       </form>
     </div>
   );
