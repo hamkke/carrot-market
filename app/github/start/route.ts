db1eedde4a02dd92607a0d47375a5f5014b6d98d
hamkke <walwalzz@naver.com>
2024-08-19 11:06:25 +0900

    #9.1 Github Authentication

---
+++ app/github/start/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const baseURL = 'https://github.com/login/oauth/authorize';
+  const params = {
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    // sns로그인하면 이메일,이름 등등 정보 제공하겠냐고 하는 부분
+    scope: 'read:user,user:email',
+    allow_signup: 'true',
+  };
+  const formattedParams = new URLSearchParams(params).toString();
+  console.log(formattedParams);
+  // client_id=Ov23liCkmCeAnpP44yz7&scope=read%3Auser%2Cuser%3Aemail&allow_signup=true
+  const finalUrl = `${baseURL}?${formattedParams}`;
+  return NextResponse.redirect(finalUrl);
+}
