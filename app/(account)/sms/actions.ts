c9a760f79442a890ec18fcdccb7f346581c7bbc1
hamkke <walwalzz@naver.com>
2024-08-26 22:51:18 +0900

    #9.5 SMS Token

--- app/(account)/sms/actions.ts
+++ app/(account)/sms/actions.ts
@@ -1,7 +1,9 @@
 'use server';
-import { z } from 'zod';
-import validator from 'validator';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
+import crypto from 'crypto';
+import validator from 'validator';
+import db from '@/lib/db';
 
 interface ActionState {
   token: boolean;
@@ -17,6 +19,22 @@ const phoneSchema = z
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
+const getToken = async () => {
+  const token = crypto.randomInt(100000, 999999).toString();
+  // Probably have the same token. So we check
+  const existsToken = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: { id: true },
+  });
+  if (existsToken) {
+    return getToken();
+  } else {
+    return token;
+  }
+};
+
 export async function smsVerification(
   prevState: ActionState,
   formData: FormData
@@ -31,6 +49,36 @@ export async function smsVerification(
         error: result.error.flatten(),
       };
     } else {
+      /**
+      사용자한테 인증번호 보내기 순서
+      1. 일단 모든 sms token을 지운다
+      2. create token
+      3. send the token using twilio
+      */
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone_number: result.data,
+          },
+        },
+      });
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone_number: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString('hex'),
+                phone_number: result.data,
+              },
+            },
+          },
+        },
+      });
       return { token: true };
     }
   } else {
